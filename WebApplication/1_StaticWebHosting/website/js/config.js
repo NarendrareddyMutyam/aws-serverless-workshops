99ed32ad932c6c7c69576bee2e00e36900fb0ce3
NarendrareddyMutyam <63656239+NarendrareddyMutyam@users.noreply.github.com>
2020-07-31 11:17:36 +0530

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // ap-southeast-1_x7oeJE3yC
+        userPoolClientId: '', // 5dks2d6iflfu17lcao8tc25erq
+        region: '' // ap-southeast-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
